6f4efc89ca9da14004b5338c93d8542f06b4c8fc
Lauticodemaster <lautigamermendoza@gmail.com>
2026-06-19 14:29:25 -0300

    Implement EntidadNoEncontradaException class

--- UTN-TUP-Porgramacion2_1PRO1-TPI_Grupo5/src/main/java/Exceptions/EntidadNoEncontradaException.java
+++ UTN-TUP-Porgramacion2_1PRO1-TPI_Grupo5/src/main/java/Exceptions/EntidadNoEncontradaException.java
@@ -1,5 +1,45 @@
 package Exceptions;
 
-public class EntidadNoEncontradaException {
+/**
+ * Se lanza cuando se busca una entidad por ID y no existe en la colección,
+ * o existe pero tiene eliminado = true (baja lógica).
+ *
+ * Ejemplos de uso:
+ *   throw new EntidadNoEncontradaException("Producto", 5L);
+ *   throw new EntidadNoEncontradaException("Usuario", id);
+ */
+public class EntidadNoEncontradaException extends RuntimeException {
 
+    private final String tipoEntidad;
+    private final Long id;
+
+    /**
+     * @param tipoEntidad Nombre de la clase buscada (ej: "Producto", "Categoria").
+     * @param id          ID que no se encontró.
+     */
+    public EntidadNoEncontradaException(String tipoEntidad, Long id) {
+        super("No se encontró " + tipoEntidad + " con id = " + id + ".");
+        this.tipoEntidad = tipoEntidad;
+        this.id = id;
+    }
+
+    /**
+     * Constructor alternativo con mensaje libre.
+     * Útil cuando el error no gira en torno a un ID numérico.
+     *
+     * @param mensaje Descripción personalizada del error.
+     */
+    public EntidadNoEncontradaException(String mensaje) {
+        super(mensaje);
+        this.tipoEntidad = null;
+        this.id = null;
+    }
+
+    public String getTipoEntidad() {
+        return tipoEntidad;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
